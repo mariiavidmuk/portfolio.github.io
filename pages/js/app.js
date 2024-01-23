85addbf432730054b65033b4b137a7dcee91427a
mariiavidmuk <vidmyk@gmail.com>
2024-01-23 16:10:28 +0100

    a

--- pages/js/app.js
+++ pages/js/app.js
@@ -1,50 +1,21 @@
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-
-if (ScrollTrigger.isTouch !== 1) {
-
-	ScrollSmoother.create({
-		wrapper: '.wrapper',
-		content: '.content',
-		smooth: 1.5,
-		effects: true
-	})
-
-	gsap.fromTo('.hero-section', { opacity: 1 }, {
-		opacity: 0,
-		scrollTrigger: {
-			trigger: '.hero-section',
-			start: 'center',
-			end: '820',
-			scrub: true
+document.addEventListener('DOMContentLoaded', function () {
+	// Add smooth scroll behavior to all internal links
+	const internalLinks = document.querySelectorAll('a[href^="#"]');
+	
+	internalLinks.forEach(link => {
+	  link.addEventListener('click', function (e) {
+		e.preventDefault();
+  
+		const targetId = this.getAttribute('href').substring(1);
+		const targetElement = document.getElementById(targetId);
+  
+		if (targetElement) {
+		  // Scroll smoothly to the target element
+		  targetElement.scrollIntoView({
+			behavior: 'smooth',
+		  });
 		}
-	})
-
-	let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
-
-	itemsL.forEach(item => {
-		gsap.fromTo(item, { opacity: 0, x: -50 }, {
-			opacity: 1, x: 0,
-			scrollTrigger: {
-				trigger: item,
-				start: '-850',
-				end: '-100',
-				scrub: true
-			}
-		})
-	})
-
-	let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
-
-	itemsR.forEach(item => {
-		gsap.fromTo(item, { opacity: 0, x: 50 }, {
-			opacity: 1, x: 0,
-			scrollTrigger: {
-				trigger: item,
-				start: '-750',
-				end: 'top',
-				scrub: true
-			}
-		})
-	})
-
-}
+	  });
+	});
+  });
+  
